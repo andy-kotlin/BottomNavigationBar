108d728b0b5160a57ccd8b064f5a468b4be6f654
amin <aminuddin01697@gmail.com>
2019-06-03 19:03:22 +0530

    Initial commit

---
+++ app/src/main/java/com/cootnet/june3project/BaseActivity.kt
@@ -0,0 +1,42 @@
+package com.cootnet.june3project
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+
+abstract  class BaseActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.first_icon -> {
+                startActivity(Intent(this, MainActivity::class.java))
+                return true
+            }
+            R.id.second_icon -> {
+                startActivity(Intent(this, Main2Activity::class.java))
+                return true
+            }
+            R.id.third_icon -> {
+                startActivity(Intent(this, Main3Activity::class.java))
+                return true
+            }
+
+        }
+        return false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getContenView())
+
+        val btnv = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        select
+        btnv.setOnNavigationItemSelectedListener(this)
+
+    }
+
+    abstract fun getContenView(): Int
+
+    abstract fun getItemId(): Int
+}
---
+++ app/src/main/java/com/cootnet/june3project/Main3Activity.kt
@@ -0,0 +1,25 @@
+package com.cootnet.june3project
+
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+
+
+class Main3Activity : BaseActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun getItemId(): Int {
+        return  R.id.third_icon
+    }
+
+    override fun getContenView(): Int {
+        return R.layout.activity_main
+    }
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
